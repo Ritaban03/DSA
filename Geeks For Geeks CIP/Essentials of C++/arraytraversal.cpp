4439ee0dcd07da6dbb34a7f8c7aa0f0f14f34d4a
Rit <2105648@kiit.ac.in>
2022-07-21 16:56:14 +0530

    array reverse traversal

---
+++ Geeks For Geeks CIP/Essentials of C++/arraytraversal.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void arrayTraversalReverse(int numbers[], int size){
+    //Write your code below
+    for(int i=size-1;i>=0;i--){
+        cout << numbers[i] << " ";
+    }
+    
+    
+    //Don't add a new line as it is provided by the driver code
+}
+
+int main()
+{
+    int arr[]={5,10,15,20};
+    
+}
