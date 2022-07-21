c7a975b88b42cefe73f9316b660e0d54a4a7248b
Rit <2105648@kiit.ac.in>
2022-07-21 19:30:11 +0530

    sum of array

---
+++ Geeks For Geeks CIP/Essentials of C++/sumofarray.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int sumof(int arr[],int n){
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum=sum+arr[i];
+    }
+    return sum;
+}
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    cout << "The sum of all elements of the array is " << sumof(arr,n);
+}
